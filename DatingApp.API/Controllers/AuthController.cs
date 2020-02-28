a7b75853bc816beaa32313ac8f57e7a9fb379d24
patrick.ntwali <patrick.ntwali@nuntius.be>
2020-02-28 19:53:45 +0200

    PNT: Implemented Security Details

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IAuthRepository authRepository,IConfiguration configuration)
+        {
+            _authRepository = authRepository;
+            _configuration = configuration;
+        }
+        // GET: api/Auth
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Auth/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Auth
+        [HttpPost("register")]
+        public async Task<IActionResult> PostAsync([FromBody]UserForRegisterDto userForRegister)
+        {
+            //validate the request.
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            userForRegister.Username = userForRegister.Username.ToLower();
+
+            if (await _authRepository.UserExists(userForRegister.Username))
+                return BadRequest("Username already exist");
+            var userToCreate = new User
+            {
+                UserName = userForRegister.Username
+            };
+            var createdUser = await _authRepository.Register(userToCreate, userForRegister.Password);
+            return StatusCode(201);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+        {
+            var userFromRepo = await _authRepository.Login(userLoginDto.Username.ToLower(), userLoginDto.Password);
+            if (userFromRepo == null)
+                return Unauthorized();
+            var claims = new[] 
+            {
+                new Claim(ClaimTypes.NameIdentifier,userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name,userFromRepo.UserName)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.
+                GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new { 
+                token = tokenHandler.WriteToken(token)
+            });
+        }
+
+        // PUT: api/Auth/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
