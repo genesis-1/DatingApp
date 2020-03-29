8698a49568c52323139b18d66b1e9aac2f47e319
patrick ntwari <patrick_ntwari@live.com>
2020-03-29 18:29:29 +0200

    PNT: Implemented Api like functionality.

---
+++ DatingApp.API/Models/Like.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Models
+{
+    public class Like
+    {
+        //this id of the user who likes other users
+        public int LikerId { get; set; }
+        //the id of the user being liked
+        public int LikeeId { get; set; }
+        public User Liker { get; set; }
+        public User Likee { get; set; }
+    }
+}
